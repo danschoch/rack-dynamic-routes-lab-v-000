6be9e5eb34fae143004b6dd5d7da511023c4b8fd
Daniel Schoch <d.schoch90@gmail.com>
2018-10-05 07:05:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
-      song = @@songs.find{|s| s.title == song_title}
+      item = @@items.find{|item| item.name == item_name}
     else
       resp.write "Route not found"
       resp.status = 400
