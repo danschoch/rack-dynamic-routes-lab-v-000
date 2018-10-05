e05e1f29120514461289aa130609f55f834800f4
Daniel Schoch <d.schoch90@gmail.com>
2018-10-05 07:03:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,12 @@ class Application
     req = Rack::Request.new(env)
     resp = Rack::Response.new
 
-    
+    if req.path.match(/items/)
+
+    else
+      resp.write "Route not found"
+      resp.status = 400
+    end
 
   end
 end
