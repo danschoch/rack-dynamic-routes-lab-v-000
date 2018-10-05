75ef4537ab034c52b790291290ff5bc2a6851a2b
Daniel Schoch <d.schoch90@gmail.com>
2018-10-05 07:15:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,8 @@ class Application
     req = Rack::Request.new(env)
     resp = Rack::Response.new
 
+    @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
