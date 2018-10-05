8596e110efa3ff9df4876f57080ca983b2acb8b5
Daniel Schoch <d.schoch90@gmail.com>
2018-10-05 07:04:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,9 @@ class Application
     resp = Rack::Response.new
 
     if req.path.match(/items/)
+      item_ = req.path.split("/songs/").last
 
+      song = @@songs.find{|s| s.title == song_title}
     else
       resp.write "Route not found"
       resp.status = 400
